7fdc4fe41c50aaff73ff0ddbfb02b8b3d97efb22
Mustapha pc <lounicimustapha6@gmail.com>
2019-05-08 08:52:46 +0100

    first version

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import PackClusters from "./components/PackClusters";
+import ForceClusters from "./components/ForceClusters";
+import defaultData from "./utils/data";
+import { GraphFromMat } from "./utils/utils";
+import LPClusturing from "./utils/LPClusturing";
+import LouvainClusturing from "./utils/LouvainClusturing";
+import Options from "./components/Options";
+// cluster using d3 pack
+// cluster using force layout
 
 function App() {
+  const [data, setData] = useState(defaultData); // adj mat
+  const [algo, setAlgo] = useState(0); // 0: LP // 1 : Louvain
+  const [graph, setGraph] = useState(null);
+  useEffect(() => {
+    const g = GraphFromMat(data);
+    let newGraph = null;
+    if (parseInt(algo) === 0) {
+      newGraph = LPClusturing(g);
+    } else {
+      newGraph = LouvainClusturing(g);
+    }
+    setGraph(newGraph);
+  }, [algo, data]);
+
+  const optionChange = (newAlgo, newData) => {
+    setAlgo(newAlgo);
+    setData(newData);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="d-flex border p-4">
+        {graph && <PackClusters graph={graph} />}
+
+        <Options algo={algo} data={data} onSubmit={optionChange} />
+      </div>
+      {graph && <ForceClusters graph={graph} />}
     </div>
   );
 }
---
+++ src/components/ForceClusters.jsx
@@ -0,0 +1,177 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useRef, useEffect } from "react";
+import * as d3 from "d3";
+// import graph from "./data.json";
+
+function ForceClusters({ graph }) {
+  const SVG = useRef(null);
+  const { nodes = {}, links = {}, clusters } = graph;
+
+  const width = 900;
+  const height = 700;
+  const radius = d3
+    .scaleLinear()
+    .domain([1, 10000])
+    .range([8, 2])(nodes.length);
+
+  useEffect(() => {
+    const createViz = (Nodes, clusters) => {
+      const nodes = Nodes;
+      d3.forceSimulation(nodes)
+        .force(
+          "link",
+          d3
+            .forceLink()
+            .links(links)
+            .distance(d => {
+              let myLength = (1 / clusters.length + 15) * radius;
+              const { source, target } = d;
+              const sourceNode = nodes.find(n => n.id === source.id);
+              const targetNode = nodes.find(n => n.id === target.id);
+              clusters.forEach(group => {
+                if (group.includes(source.id) && group.includes(target.id)) {
+                  myLength *=
+                    0.15 *
+                    (sourceNode.group.length > 1
+                      ? sourceNode.group.length * 1.2
+                      : sourceNode.group.length) *
+                    (targetNode.group.length > 1
+                      ? targetNode.group.length * 1.2
+                      : targetNode.group.length);
+                } else {
+                  myLength *= 1 / clusters.length + 1;
+                }
+              });
+
+              return myLength;
+            })
+        )
+        .force("charge", d3.forceManyBody())
+        .force("center", d3.forceCenter(width / 2, height / 2 - 100))
+        .force("collision", d3.forceCollide().radius(radius))
+        .on("tick", ticked);
+
+      function updateLinks() {
+        d3.select(".links")
+          .selectAll("line")
+          .remove();
+
+        const u = d3
+          .select(SVG.current)
+          .selectAll("line")
+          .data(links);
+
+        u.enter()
+          .append("line")
+          .merge(u)
+          .attr("x1", function(d) {
+            return d.source.x;
+          })
+          .attr("y1", function(d) {
+            return d.source.y;
+          })
+          .attr("x2", function(d) {
+            return d.target.x;
+          })
+          .attr("y2", function(d) {
+            return d.target.y;
+          });
+
+        u.exit().remove();
+      }
+
+      function updateNodes() {
+        d3.select(".nodes")
+          .selectAll("circle")
+          .remove();
+
+        var u = d3
+          .select(SVG.current)
+          .selectAll("circle")
+          .data(nodes);
+
+        u.enter()
+          .append("circle")
+          .attr("r", radius)
+          .merge(u)
+          .attr("cx", function(d) {
+            return (d.x = Math.max(radius, Math.min(width - radius, d.x)));
+          })
+          .attr("cy", function(d) {
+            return (d.y = Math.max(radius, Math.min(height - radius, d.y)));
+          })
+          .style("fill", (d, i) =>
+            d3.interpolateSinebow(
+              d3
+                .scaleLinear()
+                .domain([0, clusters.length])
+                .range([0, 1])(clusters.findIndex(g => g.includes(i)))
+            )
+          );
+
+        u.exit().remove();
+      }
+
+      function drawCluster(group) {
+        let l = [];
+        const offset = 10;
+        const groupNodes = nodes.filter(n => group.includes(n.id));
+        groupNodes.forEach(n => {
+          l.push([n.x - offset, n.y - offset]);
+          l.push([n.x - offset, n.y + offset]);
+          l.push([n.x + offset, n.y - offset]);
+          l.push([n.x + offset, n.y + offset]);
+        });
+
+        return "M" + d3.polygonHull(l).join("L") + "Z";
+      }
+
+      const updateClusters = () => {
+        d3.select(".ghull")
+          .selectAll("path.hull")
+          .remove();
+
+        d3.select(".ghull")
+          .selectAll("path.hull")
+          .data(clusters)
+          .enter()
+          .insert("path", "cicrle")
+          .attr("d", drawCluster)
+          .attr("class", "hull")
+          .style("fill", (d, i) =>
+            d3.interpolateSinebow(
+              d3
+                .scaleLinear()
+                .domain([0, clusters.length])
+                .range([0, 1])(i)
+            )
+          );
+      };
+
+      function ticked() {
+        updateLinks();
+        updateNodes();
+        updateClusters();
+      }
+    };
+    let newNodes = nodes.map(n => ({
+      ...n,
+      group: [...clusters.filter(g => g.includes(n.id)).map((g, i) => i)]
+    }));
+    if (SVG.current && graph && newNodes) {
+      createViz(newNodes, graph.clusters);
+    }
+  }, [SVG, graph]);
+
+  return (
+    <div>
+      <svg width={width} height={height} ref={SVG}>
+        <g className="links" />
+        <g className="nodes" />
+        <g className="ghull" />
+      </svg>
+    </div>
+  );
+}
+
+export default ForceClusters;
